57b64a172f468272f57aa98d03887fa1c734c973
Anbarasan7100 <canbu23@gmail.com>
2025-09-09 21:36:57 +0530

    User service Added

---
+++ user-service/src/main/java/com/skillharbour/App.java
@@ -0,0 +1,13 @@
+package com.skillharbour;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
